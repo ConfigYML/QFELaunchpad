e86707573ae23063d022b605b58865e28af4dd46
Jonabo <jonafred.boss@gmail.com>
2018-10-16 19:00:53 +0200

    Added javazoom BasicPlayer for music playback.

--- QFELaunchpad/src/music/MusicManager.java
+++ QFELaunchpad/src/music/MusicManager.java
@@ -2,11 +2,25 @@ package music;
 
 import java.io.File;
 
+import javazoom.jlgui.basicplayer.BasicPlayer;
+import javazoom.jlgui.basicplayer.BasicPlayerException;
+
 public class MusicManager {
-	public MusicManager() {
-		
-	}
+
 	public void playMusic(File f) {
-		
+		Thread t = new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				BasicPlayer player = new BasicPlayer();
+				try {
+					player.open(f);
+					player.play();
+				} catch (BasicPlayerException e) {
+					e.printStackTrace();
+				}
+			}
+		});
+		t.start();
 	}
 }
